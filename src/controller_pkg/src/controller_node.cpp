a5dbe3960cd29d45b834003814322ad4466c7160
Yichao <yichao.gao@tum.de>
2023-06-12 21:53:38 +0200

    initialize

---
+++ src/controller_pkg/src/controller_node.cpp
@@ -0,0 +1,91 @@
+#include <ros/ros.h>
+
+#include <ros/console.h>
+
+#include <tf/transform_datatypes.h>
+#include <tf_conversions/tf_eigen.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <mav_msgs/Actuators.h>
+#include <nav_msgs/Odometry.h>
+#include <trajectory_msgs/MultiDOFJointTrajectoryPoint.h>
+#include <math.h>
+#include <std_msgs/Float64.h>
+
+#define PI M_PI
+
+
+#include <eigen3/Eigen/Dense>
+
+// If you choose to use Eigen, tf provides useful functions to convert tf 
+// messages to eigen types and vice versa, have a look to the documentation:
+// http://docs.ros.org/melodic/api/eigen_conversions/html/namespacetf.html
+#include <eigen_conversions/eigen_msg.h>
+
+class controllerNode{
+  ros::NodeHandle nh;
+
+
+  ros::Subscriber current_state;
+  ros::Publisher car_commands;
+  ros::Timer timer;
+
+
+  // Controller internals (you will have to set them below)
+  // Current state
+  Eigen::Vector3d x;     // current position of the UAV's c.o.m. in the world frame
+  Eigen::Vector3d v;     // current velocity of the UAV's c.o.m. in the world frame
+  Eigen::Matrix3d R;     // current orientation of the UAV
+  Eigen::Vector3d omega; // current angular velocity of the UAV's c.o.m. in the *body* frame
+
+  // Desired state
+  Eigen::Vector3d xd;    // desired position of the UAV's c.o.m. in the world frame
+  Eigen::Vector3d vd;    // desired velocity of the UAV's c.o.m. in the world frame
+  Eigen::Vector3d ad;    // desired acceleration of the UAV's c.o.m. in the world frame
+  double yawd;           // desired yaw angle
+
+  double hz;             // frequency of the main control loop
+
+public:
+  controllerNode():hz(1000.0){
+      
+      current_state = nh.subscribe("current_state_est", 1, &controllerNode::onCurrentState, this);
+      car_commands = nh.advertise<mav_msgs::Actuators>("car_commands", 1);
+      timer = nh.createTimer(ros::Rate(hz), &controllerNode::controlLoop, this);
+  }
+
+  void onCurrentState(const nav_msgs::Odometry& cur_state){
+      
+    x << cur_state.pose.pose.position.x,cur_state.pose.pose.position.y,cur_state.pose.pose.position.z;
+    v << cur_state.twist.twist.linear.x,cur_state.twist.twist.linear.y,cur_state.twist.twist.linear.z;
+    omega << cur_state.twist.twist.angular.x,cur_state.twist.twist.angular.y,cur_state.twist.twist.angular.z;
+    Eigen::Quaterniond q;
+    tf::quaternionMsgToEigen (cur_state.pose.pose.orientation, q);
+    R = q.toRotationMatrix();
+
+
+    // Rotate omega
+    omega = R.transpose()*omega;
+  }
+
+
+  void controlLoop(const ros::TimerEvent& t){
+
+    mav_msgs::Actuators msg;
+
+    msg.angular_velocities.resize(4);
+    msg.angular_velocities[0] = 0.1; // Acceleration
+    msg.angular_velocities[1] = 0;  // Turning angle
+    msg.angular_velocities[2] = 0;  // Breaking
+    msg.angular_velocities[3] = 0;
+
+    car_commands.publish(msg);
+
+  }
+};
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "controller_node");
+  ROS_INFO_NAMED("controller", "Controller started!");
+  controllerNode n;
+  ros::spin();
+}
