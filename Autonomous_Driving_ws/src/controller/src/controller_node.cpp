511ef096fa9e5528413ae54d7025f05032972bbb
Wang, Zefeng <zefeng.wang@tum.de>
2023-07-16 13:26:57 +0200

    add pid provider node and controller node

---
+++ Autonomous_Driving_ws/src/controller/src/controller_node.cpp
@@ -0,0 +1,50 @@
+#include <ros/ros.h>
+
+#include <geometry_msgs/Twist.h>
+#include <mav_msgs/Actuators.h>
+#include <std_msgs/Float64.h>
+
+class controllerNode {
+  ros::NodeHandle nh;
+
+  ros::Subscriber twist_sub;
+  ros::Subscriber pid_effort_sub;
+
+  ros::Publisher car_commands_pub;
+
+  double linear_acc;
+  double omega;
+
+  ros::Timer timer;
+  double hz; // frequency of the main control loop
+public:
+  controllerNode() : hz(1000) {
+    twist_sub =
+        nh.subscribe("target_twist", 1, &controllerNode::updateOmega, this);
+    pid_effort_sub =
+        nh.subscribe("linear_acc", 1, &controllerNode::updateLinearAcc, this);
+    car_commands_pub = nh.advertise<mav_msgs::Actuators>("car_commands", 1);
+    timer = nh.createTimer(ros::Rate(hz), &controllerNode::controlLoop, this);
+  }
+  void updateOmega(geometry_msgs::Twist twist) { omega = twist.angular.z; }
+  void updateLinearAcc(std_msgs::Float64 effort) { linear_acc = effort.data; }
+
+  void controlLoop(const ros::TimerEvent &t) {
+
+    mav_msgs::Actuators msg;
+
+    msg.angular_velocities.resize(4);
+    msg.angular_velocities[0] = linear_acc; // Acceleration
+    msg.angular_velocities[1] = omega;      // Turning angle
+    msg.angular_velocities[2] = 0;          // Breaking
+    msg.angular_velocities[3] = 0;
+
+    car_commands_pub.publish(msg);
+  }
+};
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "controller_node");
+  controllerNode n;
+  ros::spin();
+}
---
+++ Autonomous_Driving_ws/src/controller/src/pid_provider_node.cpp
@@ -0,0 +1,62 @@
+#include "std_msgs/Float64.h"
+#include <geometry_msgs/Twist.h>
+#include <pid/pid.h>
+#include <ros/ros.h>
+
+class PidProviderNode {
+  ros::NodeHandle nh;
+
+  ros::Subscriber target_twist_sub;
+  ros::Subscriber current_twist_sub;
+
+  ros::Publisher target_v_pub;
+  ros::Publisher current_v_pub;
+
+  // ros::Publisher target_omega_pub;
+  // ros::Publisher current_omega_pub;
+public:
+  PidProviderNode() {
+    target_twist_sub = nh.subscribe("target_velocity", 1,
+                                    &PidProviderNode::extractPidTarget, this);
+    current_twist_sub = nh.subscribe(
+        "current_velocity", 1, &PidProviderNode::extractCurrentTwist, this);
+
+    target_v_pub = nh.advertise<std_msgs::Float64>("target_linear_velocity", 1);
+    current_v_pub =
+        nh.advertise<std_msgs::Float64>("current_linear_velocity", 1);
+
+    // current_omega_pub =
+    // nh.advertise<std_msgs::Float64>("current_angular_velocity", 1);
+    // target_omega_pub =
+    // nh.advertise<std_msgs::Float64>("target_angular_velocity", 1);
+  }
+  void extractPidTarget(const geometry_msgs::Twist twist) {
+    std_msgs::Float64 v;
+    v.data = twist.linear.x;
+    target_v_pub.publish(v);
+
+    // std_msgs::Float64 omega;
+    // omega = twist.angular.z;
+    // target_omega_pub.publish(omega);
+  }
+
+  void extractCurrentTwist(const geometry_msgs::Twist twist) {
+    std_msgs::Float64 v;
+    v.data = twist.linear.x;
+    current_v_pub.publish(v);
+
+    // std_msgs::Float64 omega;
+    // omega = twist.angular.z;
+    // current_omega_pub.publish(omega);
+  }
+};
+
+int main(int argc, char *argv[]) {
+  std::cout << "Initializing PidProvder node" << std::endl;
+
+  ros::init(argc, argv, "PidProviderNode");
+
+  PidProviderNode pid_provider_node;
+
+  ros::spin();
+}
