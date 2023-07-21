142237aad873e17d02206f3d617546d639578007
ZF Wang <dzfwang@outlook.com>
2023-07-21 18:22:05 +0200

    move traffic state to pid provider node

--- Autonomous_Driving_ws/src/controller/src/controller_node.cpp
+++ Autonomous_Driving_ws/src/controller/src/controller_node.cpp
@@ -11,14 +11,15 @@ class controllerNode {
   ros::Subscriber twist_sub;
   ros::Subscriber pid_effort_sub;
   ros::Subscriber traffic_state_sub;
+  ros::Subscriber breaking_sub;
 
   ros::Publisher car_commands_pub;
 
   double linear_acc;
   double omega;
-  bool stop_signal;
 
-  ros::Timer timer;
+  ros::Timer command_timer;
+  ros::Timer breaking_timer;
   double hz; // frequency of the main control loop
 public:
   controllerNode() : hz(1000) {
@@ -26,40 +27,32 @@ public:
         nh.subscribe("target_twist", 1, &controllerNode::updateOmega, this);
     pid_effort_sub =
         nh.subscribe("linear_acc", 1, &controllerNode::updateLinearAcc, this);
-    traffic_state_sub = nh.subscribe("traffic_light_state", 1,
+    traffic_state_sub = nh.subscribe("perception/traffic_state", 1,
                                      &controllerNode::updateStopSignal, this);
+    breaking_sub =
+        nh.subscribe("break_acc", 1, &controllerNode::updateBreakAcc, this);
 
     car_commands_pub = nh.advertise<mav_msgs::Actuators>("car_commands", 1);
 
-    timer = nh.createTimer(ros::Rate(hz), &controllerNode::controlLoop, this);
+    command_timer =
+        nh.createTimer(ros::Rate(hz), &controllerNode::controlLoop, this);
+    breaking_timer =
+        nh.createTimer(ros::Rate(hz), &controllerNode::breakingLoop, this);
 
-    stop_signal = false;
   }
 
   void updateOmega(geometry_msgs::Twist twist) { omega = twist.angular.z; }
 
   void updateLinearAcc(std_msgs::Float64 effort) { linear_acc = effort.data; }
 
-  void updateStopSignal(std_msgs::Bool traffic_light_state) {
-    stop_signal = traffic_light_state.data;
-  }
-
   void controlLoop(const ros::TimerEvent &t) {
     mav_msgs::Actuators msg;
 
-    if (stop_signal) {
-      msg.angular_velocities.resize(4);
-      msg.angular_velocities[0] = 0;  // Acceleration
-      msg.angular_velocities[1] = 0;  // Turning angle
-      msg.angular_velocities[2] = 10; // Breaking
-      msg.angular_velocities[3] = 0;
-    } else {
-      msg.angular_velocities.resize(4);
-      msg.angular_velocities[0] = linear_acc; // Acceleration
-      msg.angular_velocities[1] = omega;      // Turning angle
-      msg.angular_velocities[2] = 0;          // Breaking
-      msg.angular_velocities[3] = 0;
-    }
+    msg.angular_velocities.resize(4);
+    msg.angular_velocities[0] = linear_acc; // Acceleration
+    msg.angular_velocities[1] = omega;      // Turning angle
+    msg.angular_velocities[2] = 0;          // Breaking
+    msg.angular_velocities[3] = 0;
 
     car_commands_pub.publish(msg);
   }
--- Autonomous_Driving_ws/src/controller/src/pid_provider_node.cpp
+++ Autonomous_Driving_ws/src/controller/src/pid_provider_node.cpp
@@ -9,18 +9,25 @@ class PidProviderNode {
 
   ros::Subscriber target_twist_sub;
   ros::Subscriber current_state_sub;
+  ros::Subscriber traffic_state_sub;
 
   ros::Publisher target_v_pub;
   ros::Publisher current_v_pub;
 
   // ros::Publisher target_omega_pub;
   // ros::Publisher current_omega_pub;
+
+  bool stop_signal;
+  int counter;
+
 public:
   PidProviderNode() {
     target_twist_sub = nh.subscribe("target_twist", 1,
                                     &PidProviderNode::extractPidTarget, this);
     current_state_sub = nh.subscribe(
         "current_state_est", 1, &PidProviderNode::extractCurrentTwist, this);
+    traffic_state_sub = nh.subscribe("perception/traffic_state", 1,
+                                     &PidProviderNode::updateStopSignal, this);
 
     target_v_pub = nh.advertise<std_msgs::Float64>("target_linear_velocity", 1);
     current_v_pub =
@@ -31,9 +38,23 @@ public:
     // target_omega_pub =
     // nh.advertise<std_msgs::Float64>("target_angular_velocity", 1);
   }
+  void updateStopSignal(std_msgs::Bool traffic_light_state) {
+    if (traffic_light_state.data != stop_signal) {
+      counter += 1;
+    }
+    if (counter >= 5) {
+      counter = 0;
+      stop_signal = traffic_light_state.data;
+    }
+  }
   void extractPidTarget(const geometry_msgs::Twist twist) {
     std_msgs::Float64 v;
-    v.data = twist.linear.x;
+
+    if (stop_signal)
+      v.data = 0.0;
+    else
+      v.data = twist.linear.x;
+
     target_v_pub.publish(v);
 
     // std_msgs::Float64 omega;
@@ -43,7 +64,7 @@ public:
 
   void extractCurrentTwist(const nav_msgs::Odometry current_state_est) {
     std_msgs::Float64 v;
-    v.data = current_state_est.twist.twist.linear.x;
+    v.data = -current_state_est.twist.twist.linear.x;
     current_v_pub.publish(v);
 
     // std_msgs::Float64 omega;
