c4187fe89ab9dbab194acd89e20e3e64cad83966
ZF Wang <dzfwang@outlook.com>
2023-07-21 10:12:33 +0200

    change subscribed topic to odometry

--- Autonomous_Driving_ws/src/controller/src/pid_provider_node.cpp
+++ Autonomous_Driving_ws/src/controller/src/pid_provider_node.cpp
@@ -1,5 +1,6 @@
 #include "std_msgs/Float64.h"
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
 #include <pid/pid.h>
 #include <ros/ros.h>
 
@@ -7,7 +8,7 @@ class PidProviderNode {
   ros::NodeHandle nh;
 
   ros::Subscriber target_twist_sub;
-  ros::Subscriber current_twist_sub;
+  ros::Subscriber current_state_sub;
 
   ros::Publisher target_v_pub;
   ros::Publisher current_v_pub;
@@ -16,10 +17,10 @@ class PidProviderNode {
   // ros::Publisher current_omega_pub;
 public:
   PidProviderNode() {
-    target_twist_sub = nh.subscribe("target_velocity", 1,
+    target_twist_sub = nh.subscribe("target_twist", 1,
                                     &PidProviderNode::extractPidTarget, this);
-    current_twist_sub = nh.subscribe(
-        "current_velocity", 1, &PidProviderNode::extractCurrentTwist, this);
+    current_state_sub = nh.subscribe(
+        "current_state_est", 1, &PidProviderNode::extractCurrentTwist, this);
 
     target_v_pub = nh.advertise<std_msgs::Float64>("target_linear_velocity", 1);
     current_v_pub =
@@ -40,9 +41,9 @@ public:
     // target_omega_pub.publish(omega);
   }
 
-  void extractCurrentTwist(const geometry_msgs::Twist twist) {
+  void extractCurrentTwist(const nav_msgs::Odometry current_state_est) {
     std_msgs::Float64 v;
-    v.data = twist.linear.x;
+    v.data = current_state_est.twist.twist.linear.x;
     current_v_pub.publish(v);
 
     // std_msgs::Float64 omega;
