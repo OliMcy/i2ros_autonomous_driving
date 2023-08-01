c14d03d2fd2c5929f55ee16eae49ad07a725888c
Yichao <yichao.gao@tum.de>
2023-08-01 23:24:11 +0200

    add a sh file to install packages

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+sudo apt install ros-noetic-octomap-rviz-plugins ros-noetic-octomap-server
+pip install -r Autonomous_Driving_ws/src/perception/yolov5/src/yolov5/requirements.txt
+apt install ros-noetic-octomap-rviz-plugins ros-noetic-octomap-server
+apt install ros-noetic-pid ros-noetic-robot-localization
+apt install ros-noetic-smach-ros
+apt-get install ros-noetic-navigation
+apt-get install ros-noetic-teb-local-planner
+apt-get install ros-noetic-ackermann-msgs
